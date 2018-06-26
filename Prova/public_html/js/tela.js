3914ec9803fc359f417be1f19f416d169d7b16f9
Tiago Funk <tiagoff.tf@gmail.com>
2018-06-26 19:35:58 -0300

    Questoes 01 e 02

---
+++ Prova/public_html/js/tela.js
@@ -0,0 +1,29 @@
+var contador = 0;
+var listaNomes = [];
+
+$("#botaoSalvar").click(function (e) {
+    let nome = $("#nome").val();
+    console.log(nome);
+    listaNomes[contador] = nome;
+    contador++;
+});
+
+$("#botaoMostrar").click(function (e) {
+    let s = "";
+    for (let i = 0; i < listaNomes.length; i++) {
+        s += listaNomes[i] + " - ";
+    }
+    $("#mostrar").html(s);
+
+    let botaoLimpar = document.createElement("input");
+    botaoLimpar.type = "button";
+    botaoLimpar.value = "Limpar";
+
+    botaoLimpar.click(function (e) {
+        let mostrar  = document.getElementById("mostrar");
+        mostrar.value = "";
+    });
+
+    document.body.appendChild(botaoLimpar);
+});
+
